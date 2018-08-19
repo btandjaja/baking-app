510f392ece3cf900b87167095a8fd56e1449b18c
buntoro tandjaja <wen_college@yahoo.com>
2018-08-18 17:01:09 -0700

    network file added return hppt request

--- app/src/main/java/com/btandjaja/www/bakingrecipes/ListOfRecipesActivity.java
+++ app/src/main/java/com/btandjaja/www/bakingrecipes/ListOfRecipesActivity.java
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class ListOfRecipesActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class ListOfRecipesActivity extends AppCompatActivity {
+    @BindView(R.id.test_display_tv) TextView testText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_recipes);
+        ButterKnife.bind(this);
 
     }
 }
--- app/src/main/java/com/btandjaja/www/bakingrecipes/utilities/NetworkUtils.java
+++ app/src/main/java/com/btandjaja/www/bakingrecipes/utilities/NetworkUtils.java
@@ -5,10 +5,14 @@ import android.net.Uri;
 
 import com.btandjaja.www.bakingrecipes.R;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.Scanner;
 
 public class NetworkUtils {
 
@@ -36,4 +40,31 @@ public class NetworkUtils {
         }
         return url;
     }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
 }
