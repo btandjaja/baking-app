4b4903515ef0901d9724b89dabfafc4222369559
buntoro tandjaja <wen_college@yahoo.com>
2018-09-11 16:46:13 -0700

    RecipeDao, data access object, created

---
+++ app/src/main/java/com/btandjaja/www/bakingrecipes/data/RecipeDao.java
@@ -0,0 +1,28 @@
+package com.btandjaja.www.bakingrecipes.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface RecipeDao {
+    @Query("SELECT * FROM recipelist ORDER BY arrayListIndex")
+    LiveData<List<RecipeEntry>> loadAllRecipes();
+
+    @Query("SELECT * FROM recipelist WHERE id = :id")
+    LiveData<RecipeEntry> loadRecipeById(int id);
+
+    @Insert
+    void insertRecipe(RecipeEntry recipeEntry);
+
+    @Update
+    void updateRecipe(RecipeEntry recipeEntry);
+
+    @Delete
+    void deleteRecipe(RecipeEntry recipeEntry);
+}
