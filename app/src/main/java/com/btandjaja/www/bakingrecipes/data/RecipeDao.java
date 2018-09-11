57eb0bcd8b8b089ec8f7b26759fe661f6abbbdf5
buntoro tandjaja <wen_college@yahoo.com>
2018-09-11 16:47:36 -0700

    RecipeDao fixed typo

--- app/src/main/java/com/btandjaja/www/bakingrecipes/data/RecipeDao.java
+++ app/src/main/java/com/btandjaja/www/bakingrecipes/data/RecipeDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -20,7 +21,7 @@ public interface RecipeDao {
     @Insert
     void insertRecipe(RecipeEntry recipeEntry);
 
-    @Update
+    @Update(onConflict = OnConflictStrategy.REPLACE)
     void updateRecipe(RecipeEntry recipeEntry);
 
     @Delete
